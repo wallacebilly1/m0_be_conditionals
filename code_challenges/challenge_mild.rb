98e7306748723dfed0adbaf0dcdcf863cff6fe2e
Billy Wallace <wallace.billy1@gmail.com>
2024-01-13 16:09:20 -0700

    Create mild challenge code

---
+++ code_challenges/challenge_mild.rb
@@ -0,0 +1,9 @@
+# Write a Ruby program that defines a variable that stores an Integer. The program should print out the String "even" if the Integer is even, and the String "odd" if the Integer is odd.
+
+int = 9
+
+if int.even? == true
+    puts "even" 
+elsif int.even? == false
+    puts "odd"
+end
